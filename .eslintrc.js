a0bdce99b58fc3bdedafbe0230f22a4e4a7e4e48
syakoo <nichijou4009@gmail.com>
2021-02-01 15:30:08 +0900

    init: initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    semi: false,
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+  },
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { NextPage } from 'next'
+
+const HomePage: NextPage = () => <div>Hello</div>
+
+export default HomePage

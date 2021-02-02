c9a6bbb668fbbbdc3c45eca0a89dc91f0dc6a761
syakoo <nichijou4009@gmail.com>
2021-02-02 22:43:08 +0900

    add: functions of fileSystems.

---
+++ src/logics/fileSystem.ts
@@ -0,0 +1,28 @@
+import path from 'path'
+import fs from 'fs'
+
+import type { ContentDirs } from '@/types'
+
+// ___________
+//
+const contentsDir = path.join(process.cwd(), 'contents')
+
+// ___________
+//
+export const getDirs = async (dirName: ContentDirs) => {
+  const dirs = fs.readdirSync(path.join(contentsDir, dirName), {
+    withFileTypes: true,
+  })
+  const result = dirs
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
+
+  return result
+}
+
+export const readFile = async (dirName: ContentDirs, fileName: string) => {
+  const fullPath = path.join(contentsDir, dirName, fileName)
+  const result = fs.readFileSync(fullPath, 'utf-8')
+
+  return result
+}

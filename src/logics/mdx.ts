5c4a6a7455cb69c31794dc15454c55bd22186943
syakoo <nichijou4009@gmail.com>
2021-02-03 20:30:12 +0900

    update: apply prism and math renderers.

--- src/logics/mdx.ts
+++ src/logics/mdx.ts
@@ -1,9 +1,18 @@
+import rehypePrism from '@mapbox/rehype-prism'
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
+
 import renderToString from 'next-mdx-remote/render-to-string'
 
 // ___________
 //
 export const convertMdx2Source = async (content: string) => {
-  const result = await renderToString(content, {})
+  const result = await renderToString(content, {
+    mdxOptions: {
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypePrism, rehypeKatex],
+    },
+  })
 
   return result
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import { AppProps } from 'next/app'
 
 import '../../public/style.css'
+import '../../public/prism.css'
 
 // ___________
 //
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+        integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+        crossOrigin="anonymous"
+      />
       <Component {...pageProps} />
     </>
   )

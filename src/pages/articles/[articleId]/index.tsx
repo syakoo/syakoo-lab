8154f6b2ec0f1ed6579c41923292dfca65212e26
syakoo <nichijou4009@gmail.com>
2021-02-03 17:58:56 +0900

    add: an article page.

---
+++ src/pages/articles/[articleId]/index.tsx
@@ -0,0 +1,55 @@
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next'
+
+import { SingleLayout } from '@/components/layouts/SingleLayout'
+import { MarkdownRenderer } from '@/components/organisms/MarkdownRenderer'
+import type { Article } from '@/types'
+import {
+  readArticlesManifest,
+  readArticle,
+  convertMdx2Source,
+} from '@/logics/articles'
+
+// ___________
+//
+type ArticlePageProps = {
+  article: Article
+}
+
+// ___________
+//
+const ArticlePage: NextPage<ArticlePageProps> = ({ article }) => {
+  return (
+    <SingleLayout>
+      <h1>{article.title}</h1>
+      <MarkdownRenderer source={article.source} />
+    </SingleLayout>
+  )
+}
+
+// ___________
+//
+export const getStaticPaths: GetStaticPaths = async () => {
+  const articleInfos = await readArticlesManifest()
+  const paths = articleInfos.map((info) => `/articles/${info.id}`)
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps<ArticlePageProps> = async ({
+  params,
+}) => {
+  const aritlceId = params?.articleId as string
+  const { data, content } = await readArticle(aritlceId)
+  const source = await convertMdx2Source(content)
+  const article = {
+    ...data,
+    source,
+  } as Article
+
+  return { props: { article } }
+}
+
+export default ArticlePage

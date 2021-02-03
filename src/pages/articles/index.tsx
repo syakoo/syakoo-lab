29b1860ffc295d29f0e50335957fb4f093210745
syakoo <nichijou4009@gmail.com>
2021-02-03 10:33:27 +0900

    update: an article page.

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,13 +1,46 @@
 import { NextPage } from 'next'
 
 import { SingleLayout } from '@/components/layouts/SingleLayout'
+import type { Article } from '@/types'
+
+import {
+  readArticlesManifest,
+  readArticle,
+  convertMdx2Source,
+} from '@/logics/articles'
 
 // ___________
 //
-const ArticlesPage: NextPage = () => (
-  <SingleLayout>
-    <h2>Articles Page</h2>
-  </SingleLayout>
-)
+type ArticlesPageProps = {
+  articles: Article[]
+}
+
+// ___________
+//
+const ArticlesPage: NextPage<ArticlesPageProps> = ({ articles }) => {
+  return (
+    <SingleLayout>
+      <h2>Articles Page</h2>
+      {articles.map((article) => (
+        <p key={article.id}>{article.title}</p>
+      ))}
+    </SingleLayout>
+  )
+}
+
+// ___________
+//
+export const getStaticProps = async () => {
+  const posts = await readArticlesManifest()
+  const articles: Article[] = await Promise.all(
+    posts.map(async (post) => {
+      const { content } = await readArticle(`${post.id}`)
+      const source = await convertMdx2Source(content)
+      return { ...post, source }
+    })
+  )
+
+  return { props: { articles } }
+}
 
 export default ArticlesPage

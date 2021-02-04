d4cb8c29fe8d8372da8d335d6df28f1a422dc9fc
syakoo <nichijou4009@gmail.com>
2021-02-04 13:32:32 +0900

    add: types of aboutme.

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,11 @@ import type { MdxRemote } from 'next-mdx-remote/types'
 
 // ___________
 //
-export type ContentDirs = 'articles'
+export type ContentDirs = 'articles' | 'aboutme'
+export type Source = MdxRemote.Source
 
+// ___________
+// Article
 export interface ArticleInfo {
   title: string
   published: string
@@ -12,5 +15,22 @@ export interface ArticleInfo {
 }
 
 export interface Article extends ArticleInfo {
-  source: MdxRemote.Source
+  source: Source
+}
+
+// ___________
+// AboutMe
+export interface AboutMeLink {
+  name: string
+  url: string
+  imgUrl: string
+}
+export interface AboutMeInfo {
+  name: string
+  imgUrl: string
+  links: AboutMeLink[]
+  certificates: string[]
+  timeline: {
+    [k: string]: string[]
+  }
 }

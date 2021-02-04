bad2f636cc14178085f23ae08c4d7e8171fb6202
syakoo <nichijou4009@gmail.com>
2021-02-04 13:33:17 +0900

    update: a me page to apply data.

--- src/components/organisms/AboutMe/AboutMe.tsx
+++ src/components/organisms/AboutMe/AboutMe.tsx
@@ -1,13 +1,55 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import { Card } from '@/components/atoms/Card'
+import { MarkdownRenderer } from '@/components/organisms/MarkdownRenderer'
+import type { Source, AboutMeLink } from '@/types'
 
 // import styles from './styles.module.scss'
 
 // ___________
 //
-const AboutMe: React.VFC = () => {
-  return <Card title="About Me">About Me</Card>
+type AboutMeProps = {
+  descriptionSource: Source
+  name: string
+  imgUrl: string
+  links: AboutMeLink[]
+}
+
+// ___________
+//
+const AboutMe: React.VFC<AboutMeProps> = ({
+  descriptionSource,
+  name,
+  imgUrl,
+  links,
+}) => {
+  return (
+    <Card title="About Me">
+      <div>
+        <Image src={imgUrl} alt="me" width={120} height={120} />
+        <div>
+          <h3>{name}</h3>
+          <div>
+            {links.map((link) => (
+              <Link key={link.name} href={link.url}>
+                <Image
+                  src={link.imgUrl}
+                  alt={link.name}
+                  width={24}
+                  height={24}
+                />
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div>
+        <MarkdownRenderer source={descriptionSource} />
+      </div>
+    </Card>
+  )
 }
 
 export default AboutMe
--- src/pages/about/me.tsx
+++ src/pages/about/me.tsx
@@ -1,14 +1,45 @@
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 
 import { SingleLayout } from '@/components/layouts/SingleLayout'
 import { AboutMe } from '@/components/organisms/AboutMe'
+import { getAboutMeInfo, getAboutMeSource } from '@/logics/aboutme'
+import type { AboutMeInfo, Source } from '@/types'
 
 // ___________
 //
-const AboutMePage: NextPage = () => (
+type AboutMePageProps = {
+  aboutmeInfo: AboutMeInfo
+  descriptionSource: Source
+}
+
+// ___________
+//
+const AboutMePage: NextPage<AboutMePageProps> = ({
+  aboutmeInfo,
+  descriptionSource,
+}) => (
   <SingleLayout>
-    <AboutMe />
+    <AboutMe
+      name={aboutmeInfo.name}
+      imgUrl={aboutmeInfo.imgUrl}
+      descriptionSource={descriptionSource}
+      links={aboutmeInfo.links}
+    />
   </SingleLayout>
 )
+
+// ___________
+//
+export const getStaticProps: GetStaticProps<AboutMePageProps> = async () => {
+  const info = await getAboutMeInfo()
+  const source = await getAboutMeSource()
+
+  return {
+    props: {
+      aboutmeInfo: info,
+      descriptionSource: source,
+    },
+  }
+}
 
 export default AboutMePage

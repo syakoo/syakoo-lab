62cd4596e6ce6b8cd16668583d0d65e310592931
syakoo <nichijou4009@gmail.com>
2021-02-05 13:39:20 +0900

    update: chagne path about/me to about-me.

--- src/components/templates/Header/navData.ts
+++ src/components/templates/Header/navData.ts
@@ -8,7 +8,7 @@ type NavData = ANavData[]
 // ___________
 //
 export const navData: NavData = [
-  { url: '/about/me', title: 'Aboutme' },
+  { url: '/about-me', title: 'Aboutme' },
   { url: '/apps', title: 'Apps' },
   { url: '/articles', title: 'Articles' },
   { url: '/arts', title: 'Arts' },

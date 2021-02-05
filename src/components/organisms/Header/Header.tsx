bc07d87bcb9c6da2f8eff136dce179656feadf27
syakoo <nichijou4009@gmail.com>
2021-02-05 13:04:07 +0900

    update: nav of a Header comp.

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 import styles from './styles.module.scss'
@@ -7,13 +8,15 @@ import { navData } from './navData'
 // ___________
 //
 const Header: React.VFC = () => {
+  const { route } = useRouter()
+
   return (
     <header className={styles.Header}>
       <div>Syakoo&apos;s Lab</div>
       <nav>
         {navData.map((d) => (
           <Link href={d.url} key={d.title}>
-            {d.title}
+            <a data-is-activate={route.match(d.url)}>{d.title}</a>
           </Link>
         ))}
       </nav>

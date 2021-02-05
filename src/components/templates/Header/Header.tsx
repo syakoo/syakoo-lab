96fcf00d5a7196d79cea68c1e71658054173cfc6
syakoo <nichijou4009@gmail.com>
2021-02-05 14:11:57 +0900

    update: a Header comp.

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
+import { TitleSVG } from '@/components/atoms/TitleSVG'
+
 import styles from './styles.module.scss'
 import { navData } from './navData'
 
@@ -12,14 +14,22 @@ const Header: React.VFC = () => {
 
   return (
     <header className={styles.Header}>
-      <div>Syakoo&apos;s Lab</div>
-      <nav>
-        {navData.map((d) => (
-          <Link href={d.url} key={d.title}>
-            <a data-is-activate={route.match(d.url)}>{d.title}</a>
+      <div className={styles.inner}>
+        <div className={styles.title}>
+          <Link href="/">
+            <a>
+              <TitleSVG />
+            </a>
           </Link>
-        ))}
-      </nav>
+        </div>
+        <nav>
+          {navData.map((d) => (
+            <Link href={d.url} key={d.title}>
+              <a data-is-activate={route.match(d.url)}>{d.title}</a>
+            </Link>
+          ))}
+        </nav>
+      </div>
     </header>
   )
 }

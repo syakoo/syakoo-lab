535ac074dd8dff645b787cb2c8635accd11aa8a3
syakoo <nichijou4009@gmail.com>
2021-02-06 13:13:02 +0900

    add: hygen command to generate new article.

--- .hygen.js
+++ .hygen.js
@@ -1,3 +1,17 @@
+const zfill = (n) => ('00' + n).slice(-2)
+
 module.exports = {
   templates: `${__dirname}/.hygen`,
-};
+  helpers: {
+    now: () => {
+      const d = new Date()
+      return `${d.getFullYear()}-${zfill(d.getMonth() + 1)}-${zfill(
+        d.getDate()
+      )}`
+    },
+    dirName: () => {
+      const d = new Date()
+      return `${d.getFullYear()}${zfill(d.getMonth() + 1)}${zfill(d.getDate())}`
+    },
+  },
+}

f3255ecbed935cda6f11f0e991f7e56a8cc0728e
Takuya Sakoda <syakoo.pub@gmail.com>
2023-09-22 18:50:49 +0900

    ✨ feat(home): [WIP] impl Home page

---
+++ src/features/home/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import "@/globalStyle.css";
+import { Header } from "@/features/common/components/Header";
+
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};

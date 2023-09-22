5d25b754fb3893dea1fc4d05271dbc041b6762c8
Takuya Sakoda <syakoo.pub@gmail.com>
2023-09-22 17:22:23 +0900

    ✨ feat(common/components): impl SyakooLabText component

---
+++ src/common/components/SyakooLabText/SyakooLabText.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { SyakooLabText } from ".";
+
+const meta = {
+  component: SyakooLabText,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof SyakooLabText>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Sample: Story = {};
---
+++ src/common/components/SyakooLabText/index.ts
@@ -0,0 +1,1 @@
+export { SyakooLabText } from "./SyakooLabText";

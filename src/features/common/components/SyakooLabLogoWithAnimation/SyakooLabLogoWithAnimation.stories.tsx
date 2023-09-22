e8c3fb9b7f8137f33ddc6801b4f90259b015706a
Takuya Sakoda <syakoo.pub@gmail.com>
2023-09-22 18:10:21 +0900

    ✨ feat(common/components): impl SyakooLabLogoWithAnimation component

---
+++ src/features/common/components/SyakooLabLogoWithAnimation/SyakooLabLogoWithAnimation.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { SyakooLabLogoWithAnimation } from ".";
+
+const meta = {
+  component: SyakooLabLogoWithAnimation,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof SyakooLabLogoWithAnimation>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Sample: Story = {};
---
+++ src/features/common/components/SyakooLabLogoWithAnimation/index.ts
@@ -0,0 +1,1 @@
+export { SyakooLabLogoWithAnimation } from "./SyakooLabLogoWithAnimation";

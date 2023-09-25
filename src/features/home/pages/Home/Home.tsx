ed1afd6506e5e68110a40b29ced1b2ea18caa312
Takuya Sakoda (syakoo) <syakoo.pub@gmail.com>
2023-09-25 09:02:23 +0900

    ✨ feat(home): [WIP] update Home page component

--- src/features/home/pages/Home/Home.tsx
+++ src/features/home/pages/Home/Home.tsx
@@ -1,5 +1,23 @@
+import { Center, Container } from "@/design-system/layout";
+import { H2, Text } from "@/design-system/ui";
 import { HeaderFooterTemplate } from "@/features/common/components/HeaderFooterTemplate";
+import { SyakooLabLogoWithAnimation } from "@/features/common/components/SyakooLabLogoWithAnimation";
 
 export const Home: React.FC = () => {
-  return <HeaderFooterTemplate>Home page</HeaderFooterTemplate>;
+  return (
+    <HeaderFooterTemplate>
+      <Center>
+        <Container as="main" paddingTop="200" paddingX="200" size="100">
+          <Center>
+            <SyakooLabLogoWithAnimation height="140px" />
+          </Center>
+          <Text>このサイトは現在作成中です。</Text>
+        </Container>
+        <Container paddingTop="200" paddingX="200" size="100">
+          <H2>About Me</H2>
+          <Text>hi</Text>
+        </Container>
+      </Center>
+    </HeaderFooterTemplate>
+  );
 };

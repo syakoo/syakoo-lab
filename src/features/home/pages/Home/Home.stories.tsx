0ef3e2a549b6aa79a75f1f38a48f99aa8be21331
Takuya Sakoda <syakoo.pub@gmail.com>
2023-09-25 17:06:45 +0900

    ✨ feat(home/Home): update Home page component

---
+++ src/features/home/pages/Home/Home.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Home } from ".";
+
+const meta = {
+  component: Home,
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof Home>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Sample: Story = {};
--- src/features/home/pages/Home/Home.tsx
+++ src/features/home/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import "@/features/common/logics/globalSettings";
-import { Center, Container } from "@/design-system/layout";
+import { AboutMe } from "./AboutMe";
+import { Center, Col, Container } from "@/design-system/layout";
 import { H2, Text } from "@/design-system/ui";
 import { HeaderFooterTemplate } from "@/features/common/components/HeaderFooterTemplate";
 import { SyakooLabLogoWithAnimation } from "@/features/common/components/SyakooLabLogoWithAnimation";
@@ -7,18 +8,27 @@ import { SyakooLabLogoWithAnimation } from "@/features/common/components/SyakooL
 export const Home: React.FC = () => {
   return (
     <HeaderFooterTemplate>
-      <Center>
-        <Container as="main" paddingTop="200" paddingX="200" size="100">
-          <Center>
-            <SyakooLabLogoWithAnimation height="140px" />
-          </Center>
-          <Text>このサイトは現在作成中です。</Text>
+      <Col align="stretch" gap="400">
+        <Container as="main" center paddingTop="200" paddingX="200" size="100">
+          <Col gap="200">
+            <Center>
+              <SyakooLabLogoWithAnimation height="140px" />
+            </Center>
+            <Center>
+              <Text>
+                このサイトは syakoo 個人のサイトです。
+                作成したアプリやイラスト、記事を公開しています。
+              </Text>
+            </Center>
+          </Col>
         </Container>
-        <Container paddingTop="200" paddingX="200" size="100">
-          <H2>About Me</H2>
-          <Text>hi</Text>
+        <Container as="section" center paddingX="200" size="100">
+          <Col gap="100">
+            <H2>About Me</H2>
+            <AboutMe />
+          </Col>
         </Container>
-      </Center>
+      </Col>
     </HeaderFooterTemplate>
   );
 };

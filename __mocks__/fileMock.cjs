4e159811c1a6a9484208d059bc18494dae2973c8
Takuya Sakoda <syakoo.pub@gmail.com>
2023-09-30 12:18:11 +0900

    🔧 chore(jest): impl jestImageTransformer and setting transformers

---
+++ __mocks__/fileMock.cjs
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const jestImageTransformer = require("../src/libs/astro-image/jestImageTransformer.cjs");
+
+module.exports = jestImageTransformer;
--- jest.config.cjs
+++ jest.config.cjs
@@ -16,5 +16,8 @@ module.exports = {
         tsconfig: "tsconfig.jest.json",
       },
     ],
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.cjs",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.cjs",
   },
 };

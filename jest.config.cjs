398e26255ea29daed625aec5c63c186ebff1d510
Takuya Sakoda <syakoo.pub@gmail.com>
2023-09-30 10:58:20 +0900

    🔧 chore(jest): install and setup jest,react

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+
+/** @type{import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+};

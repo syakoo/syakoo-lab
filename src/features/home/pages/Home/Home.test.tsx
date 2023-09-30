3d51ba384b743dff420037fa51a8c9a53fe6efe9
Takuya Sakoda <syakoo.pub@gmail.com>
2023-09-30 12:19:22 +0900

    ✨ feat(test): add component snapshot tests

---
+++ src/features/home/pages/Home/Home.test.tsx
@@ -0,0 +1,21 @@
+import { composeStories } from "@storybook/react";
+import { act, render } from "@testing-library/react";
+import * as stories from "./Home.stories";
+
+const { ...otherStories } = composeStories(stories);
+
+describe("Home", () => {
+  const testCases = Object.values(otherStories).map(
+    (Story) => [Story.storyName, Story] as const,
+  );
+  test.each(testCases)("renders %s", async (_, Story) => {
+    const tree = render(<Story />);
+    await act(async () => {
+      if (Story.play) {
+        await Story.play({ canvasElement: tree.container });
+      }
+    });
+
+    expect(tree.baseElement).toMatchSnapshot();
+  });
+});

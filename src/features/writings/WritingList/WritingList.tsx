61b5e72c5ce982f634074890577ea90df52d42fc
Takuya Sakoda <syakoo.pub@gmail.com>
2023-10-10 22:42:57 +0900

    ♻️ refactor(writings): move template to page component

--- src/features/writings/WritingList/WritingList.tsx
+++ src/features/writings/WritingList/WritingList.tsx
@@ -2,9 +2,8 @@ import { WritingBlock } from "./WritingBlock";
 import { WritingTab } from "./WritingTab";
 import { WritingTypeDescription } from "./WritingTypeDescription";
 import type { WritingListType } from "./shared/writingListType";
-import { Col, Container } from "@/design-system/layout";
+import { Col } from "@/design-system/layout";
 import { H2 } from "@/design-system/ui";
-import { HeaderFooterTemplate } from "@/features/common/components/HeaderFooterTemplate";
 import type { WritingMeta } from "@/features/writings/types";
 
 type WritingListProps = {
@@ -19,28 +18,18 @@ export const WritingList: React.FC<WritingListProps> = ({ type, metas }) => {
   });
 
   return (
-    <HeaderFooterTemplate>
-      <main>
-        <Container
-          as="section"
-          center
-          paddingBottom="400"
-          paddingX="200"
-          size="100"
-        >
-          <Col gap="100">
-            <H2>Writings</H2>
-            <WritingTab selectedType={type} />
-            <WritingTypeDescription type={type} />
-            {/* TODO: 空白を入れたい */}
-            <Col gap="200">
-              {filteredMetas.map((meta) => (
-                <WritingBlock key={meta.id} meta={meta} />
-              ))}
-            </Col>
-          </Col>
-        </Container>
-      </main>
-    </HeaderFooterTemplate>
+    <section>
+      <Col gap="100">
+        <H2>Writings</H2>
+        <WritingTab selectedType={type} />
+        <WritingTypeDescription type={type} />
+        {/* TODO: 空白を入れたい */}
+        <Col gap="200">
+          {filteredMetas.map((meta) => (
+            <WritingBlock key={meta.id} meta={meta} />
+          ))}
+        </Col>
+      </Col>
+    </section>
   );
 };

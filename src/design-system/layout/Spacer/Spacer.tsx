5b768ab305f68695caaac021283f04b888698525
Takuya Sakoda <syakoo.pub@gmail.com>
2023-10-14 19:03:16 +0900

    ✨ feat(design-system/layout): impl Spacer component

---
+++ src/design-system/layout/Spacer/Spacer.tsx
@@ -0,0 +1,8 @@
+import type { RecipeVariants } from "@vanilla-extract/recipes";
+import { spacerStyle } from "./Spacer.css";
+
+type SpacerProps = RecipeVariants<typeof spacerStyle>;
+
+export const Spacer: React.FC<SpacerProps> = (props) => {
+  return <div aria-hidden className={spacerStyle(props)} />;
+};

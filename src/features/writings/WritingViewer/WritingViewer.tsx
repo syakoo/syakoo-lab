35091c35c02fa6f0f58e6c2216413933c36fa803
Takuya Sakoda <syakoo.pub@gmail.com>
2023-10-22 13:10:12 +0900

    ✨ feat(writings): add Link to mdx parts

--- src/features/writings/WritingViewer/WritingViewer.tsx
+++ src/features/writings/WritingViewer/WritingViewer.tsx
@@ -2,10 +2,16 @@ import type { Writing } from "../types";
 import { WritingHeader } from "./WritingHeader";
 import { writingViewerStyles } from "./WritingViewer.css";
 import { mdxParts } from "./mdxParts/index";
+import { Link } from "@/design-system/ui";
 import { resolveMDXAsComponent } from "@/features/mdx/resolveMDXAsComponent";
 
 type WritingViewerProps = {
   writing: Writing;
+};
+
+const components = {
+  ...mdxParts,
+  a: (props: Parameters<typeof Link>[0]) => <Link colored {...props} />,
 };
 
 export const WritingViewer: React.FC<WritingViewerProps> = ({ writing }) => {
@@ -17,7 +23,7 @@ export const WritingViewer: React.FC<WritingViewerProps> = ({ writing }) => {
         <WritingHeader meta={writing.meta} />
       </div>
       <div className={writingViewerStyles.contentWrapper}>
-        <MDXComponent components={mdxParts} />
+        <MDXComponent components={components} />
       </div>
       <aside className={writingViewerStyles.asideWrapper}>
         TOC

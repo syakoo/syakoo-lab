0cbf4ad9b1f787a94109107981672b781deaa0b2
Takuya Sakoda <syakoo.pub@gmail.com>
2023-10-24 22:54:55 +0900

    ✨ feat(writings): centering WritingHeader contents

--- src/features/writings/WritingViewer/WritingHeader/WritingHeader.tsx
+++ src/features/writings/WritingViewer/WritingHeader/WritingHeader.tsx
@@ -1,7 +1,7 @@
 import { writingTypeConfig } from "../../_shared/writingType";
 import type { WritingMeta } from "../../types";
 import { writingHeaderStyles } from "./WritingHeader.css";
-import { Col, Row, Spacer } from "@/design-system/layout";
+import { Center, Col, Row, Spacer } from "@/design-system/layout";
 import { H1, Link, Text } from "@/design-system/ui";
 import { Icon } from "@/features/icons";
 
@@ -20,20 +20,24 @@ export const WritingHeader: React.FC<WritingHeaderProps> = ({ meta }) => {
         <Icon name={iconName} width="100%" />
       </div>
       <Spacer y="100" />
-      <H1>{meta.title}</H1>
+      <div className={writingHeaderStyles.titleWrapper}>
+        <H1>{meta.title}</H1>
+      </div>
       <Spacer y="100" />
       <Col gap="25">
-        <Row gap="50">
+        <Row gap="50" justify="center">
           {meta.tags.map((tag) => (
             <Text key={tag} color="secondary" size="50">
               #{tag}
             </Text>
           ))}
         </Row>
-        <Text color="secondary" size="75">
-          <Link href="/">syakoo</Link> posted on{" "}
-          <time dateTime={meta.published}>{meta.published}</time>
-        </Text>
+        <Center>
+          <Text color="secondary" size="75">
+            <Link href="/">syakoo</Link> posted on{" "}
+            <time dateTime={meta.published}>{meta.published}</time>
+          </Text>
+        </Center>
       </Col>
     </header>
   );

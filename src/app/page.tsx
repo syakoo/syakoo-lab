621b79405716c0ffcd60d8452095be3d9c4e4a2a
Takuya Sakoda <syakoo.pub@gmail.com>
2023-10-28 17:08:23 +0900

    ♻️ refactor(home): destruct home page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-import { Home } from "@/features/home/pages/Home";
+import { Col, Container } from "@/design-system/layout";
+import { H2 } from "@/design-system/ui";
+import { HeaderFooterTemplate } from "@/features/common/components/HeaderFooterTemplate";
+import { AboutMe } from "@/features/home/AboutMe";
+import { Home } from "@/features/home/Home";
 
 const HomePage = () => {
-  return <Home />;
+  return (
+    <HeaderFooterTemplate>
+      <Col align="stretch" gap="500">
+        <Container as="main" center paddingTop="400" paddingX="200" size="100">
+          <Home />
+        </Container>
+        <Container
+          as="section"
+          center
+          paddingBottom="400"
+          paddingX="200"
+          size="100"
+        >
+          <Col gap="100">
+            <H2>About Me</H2>
+            <AboutMe />
+          </Col>
+        </Container>
+      </Col>
+    </HeaderFooterTemplate>
+  );
 };
 
 export default HomePage;

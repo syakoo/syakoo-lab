17342622da3eaa5a5e6fd93ccb32231f63be7305
Takuya Sakoda <syakoo.pub@gmail.com>
2023-10-28 11:31:58 +0900

    ✨ feat: impl sample next page

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Syakoo Lab",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main>
+      <h1>hello world</h1>
+    </main>
+  );
+};
+
+export default HomePage;

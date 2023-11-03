045eefe5e1ecc32ec4d214dfc503f81b0c74e3b6
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-03 23:03:38 +0900

    ✨ feat(writings/WritingViewer): update WritingHeader

--- src/features/writings/WritingViewer/WritingHeader/WritingHeader.tsx
+++ src/features/writings/WritingViewer/WritingHeader/WritingHeader.tsx
@@ -2,7 +2,7 @@ import { writingTypeConfig } from "../../_shared/writingType";
 import type { WritingMeta } from "../../types";
 import { writingHeaderStyles } from "./WritingHeader.css";
 import { Center, Col, Row, Spacer } from "@/design-system/layout";
-import { H1, Link, Text } from "@/design-system/ui";
+import { H1, Link, Span, Text } from "@/design-system/ui";
 import { Icon } from "@/features/icons";
 
 type WritingHeaderProps = {
@@ -25,18 +25,31 @@ export const WritingHeader: React.FC<WritingHeaderProps> = ({ meta }) => {
       </div>
       <Spacer y="100" />
       <Col gap="25">
+        <Center>
+          <Link href="/">
+            <Span color="secondary" size="75">
+              syakoo
+            </Span>
+          </Link>
+        </Center>
         <Row gap="50" justify="center">
           {meta.tags.map((tag) => (
-            <Text key={tag} color="secondary" size="50">
+            <Text key={tag} color="secondary" size="75">
               #{tag}
             </Text>
           ))}
         </Row>
         <Center>
-          <Text color="secondary" size="75">
-            <Link href="/">syakoo</Link> posted on{" "}
-            <time dateTime={meta.published}>{meta.published}</time>
-          </Text>
+          <div>
+            <Span color="secondary" size="75">
+              投稿日 <time dateTime={meta.published}>{meta.published}</time>
+            </Span>
+            {meta.updated ? (
+              <Span color="secondary" size="75">
+                ・最終更新日 {meta.updated}
+              </Span>
+            ) : null}
+          </div>
         </Center>
       </Col>
     </header>

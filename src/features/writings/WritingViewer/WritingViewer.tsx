71edc161ceabf1ace9649b4d2aa7f9f63f34bd19
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-04 10:40:09 +0900

    ✨ feat(writings/WritingViewer): impl old writing notation

--- src/features/writings/WritingViewer/WritingViewer.tsx
+++ src/features/writings/WritingViewer/WritingViewer.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { addYears, isBefore } from "date-fns";
 import type { Writing } from "../types";
 import { TOC } from "./TOC";
 import { WritingHeader } from "./WritingHeader";
 import { writingViewerStyles } from "./WritingViewer.css";
+import { Note } from "./mdxParts/Note";
 import { mdxParts } from "./mdxParts/index";
 import { Link, FadeIn } from "@/design-system/ui";
 import { useMermaid } from "@/features/mdx/plugins/mermaid/useMermaid";
@@ -23,6 +25,12 @@ export const WritingViewer: React.FC<WritingViewerProps> = ({ writing }) => {
   useMermaid();
   useTwitter();
   const MDXComponent = resolveMDXAsComponent(writing.serializedBody);
+  const isOldWriting = (() => {
+    // NOTE: diary の場合は古くても表示する必要はないと判断
+    if (writing.meta.type === "diary") return false;
+
+    return isBefore(new Date(writing.meta.published), addYears(new Date(), -1));
+  })();
 
   return (
     <FadeIn>
@@ -31,6 +39,11 @@ export const WritingViewer: React.FC<WritingViewerProps> = ({ writing }) => {
           <WritingHeader meta={writing.meta} />
         </div>
         <div className={writingViewerStyles.contentWrapper}>
+          {isOldWriting ? (
+            <Note variant="warn">
+              この記事は古い内容である可能性があります。
+            </Note>
+          ) : null}
           <MDXComponent components={components} />
         </div>
         <aside className={writingViewerStyles.asideWrapper}>

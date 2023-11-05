7ad8d2ccfd49154aa1892f097515afae6bf9def6
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-05 12:33:17 +0900

    🔧 chore(next.config): fix vanillaExtractPlugin and set static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+};
 
-export default createVanillaExtractPlugin(nextConfig);
+export default createVanillaExtractPlugin()(nextConfig);

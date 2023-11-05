89415cdda4a27de939bad5519a5ff3139652c4a5
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-05 12:47:21 +0900

    🔧 chore(next.config): set unoptimized image

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: { unoptimized: true },
 };
 
 export default createVanillaExtractPlugin()(nextConfig);

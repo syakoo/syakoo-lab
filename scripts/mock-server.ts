999cdd75577b3245883fecb7de9ff3c20dbbdca7
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-12 15:31:05 +0900

    🔧 chore: setting msw x jest config

--- scripts/mock-server.ts
+++ scripts/mock-server.ts
@@ -5,5 +5,5 @@ import dotenv from "dotenv";
 dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
 
 // NOTE: 動的にすることで読むタイミングを遅らせる
-const { runServer } = await import("@/api/_mocks/msw");
+const { runServer } = await import("@/api/_mocks/server");
 runServer();
--- src/api/_mocks/jest.ts
+++ src/api/_mocks/jest.ts
@@ -1,6 +1,9 @@
 import type { RequestHandler } from "msw";
 import { setupServer } from "msw/node";
 
+/**
+ * jest 用のモックサーバー起動関数
+ */
 export function setupJestMockServer(...handlers: RequestHandler[]) {
   const server = setupServer(...handlers);
   beforeAll(() => server.listen());

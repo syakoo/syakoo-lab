c2f7b27f6b2f0dcb98eb3c2f4f7a8b573d33a985
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-12 15:31:31 +0900

    ✨ feat(arts/ArtSection): impl fav button

--- src/features/arts/ArtSection/ArtSection.css.ts
+++ src/features/arts/ArtSection/ArtSection.css.ts
@@ -6,6 +6,9 @@ export const artSectionStyles = {
   root: style({
     paddingBlock: tokens.spaces[400],
   }),
+  heartButtonWrapper: style({
+    width: 36,
+  }),
   imageWrapper: style({
     width: "100%",
     maxWidth: tokens.sizes[100],
@@ -18,8 +21,11 @@ export const artSectionStyles = {
   body: style({
     maxWidth: "500px",
     margin: "auto",
-    marginTop: tokens.spaces[400],
+    marginTop: tokens.spaces[300],
     paddingInline: tokens.spaces[200],
     width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    gap: tokens.spaces[100],
   }),
 };

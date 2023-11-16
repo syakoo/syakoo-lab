026b7678317ad3f45189e64b1c9d45973272b0b1
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-16 09:16:21 +0900

    ✨ feat(app/layout): add katex stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,10 @@ export default function RootLayout({
       lang="ja"
     >
       <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css"
+          rel="stylesheet"
+        />
         <Suspense>
           <GoogleAnalytics />
         </Suspense>

211b2480ab1d4ebb43d017a7661e614fe4c32be4
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-23 23:07:02 +0900

    ♻️ refactor(mdx/plugins): update run initial effect as useMount

--- src/features/mdx/plugins/twitter/useTwitter.ts
+++ src/features/mdx/plugins/twitter/useTwitter.ts
@@ -1,14 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useMount } from "@/utils/mount/useMount";
 
 export const useTwitter = () => {
-  const isLoadedRef = useRef<boolean>(false);
-
-  useEffect(() => {
-    if (!isLoadedRef.current) {
-      const s = document.createElement("script");
-      s.src = "https://platform.twitter.com/widgets.js";
-      document.body.appendChild(s);
-      isLoadedRef.current = true;
-    }
-  }, []);
+  useMount(() => {
+    const s = document.createElement("script");
+    s.src = "https://platform.twitter.com/widgets.js";
+    document.body.appendChild(s);
+  });
 };

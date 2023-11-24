1c01988c3d2c77d5762ef14dc958c43529c16b93
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-24 17:30:11 +0900

    ✨ feat(writings): impl finding related writing logic

--- src/features/writings/RelatedWritingsNav/findRelatedWritingHeads.ts
+++ src/features/writings/RelatedWritingsNav/findRelatedWritingHeads.ts
@@ -1,14 +1,36 @@
+import { compareDesc } from "date-fns";
+
 import { readWritingContents } from "@/contents/writings/reader";
 import { resolveWritingHead } from "@/features/writings/_models/headResolver";
 import { type WritingHead } from "@/features/writings/_models/types";
-import { isIntersect } from "@/utils/array/isIntersect";
+
+const writingRelatedScore =
+  (originalTags: WritingHead["tags"]) => (targetTags: WritingHead["tags"]) => {
+    return targetTags.reduce((prev, tag, i) => {
+      const j = originalTags.findIndex((t) => t === tag);
+      if (j === -1) return prev;
+      // NOTE: 雑。10 もこれ以上タグは増えないだろうという決め打ちによるもの。
+      return prev + (10 - i) * (10 - j);
+    }, 0);
+  };
 
 /**
  * そのタグに関連する文章の一覧を取得する関数
  */
-export const findRelatedWritingHeads = async (tags: WritingHead["tags"]) => {
-  return (await readWritingContents())
-    .map(({ frontMatter }) => resolveWritingHead(frontMatter))
-    .filter((head) => isIntersect(head.tags, tags))
-    .slice(0, 5);
+export const findRelatedWritingHeads = async (head: WritingHead) => {
+  const score = writingRelatedScore(head.tags);
+
+  return (
+    (await readWritingContents())
+      .map(({ frontMatter }) => resolveWritingHead(frontMatter))
+      // NOTE: そいつ自身は抜かす
+      .filter(({ id }) => id !== head.id)
+      // NOTE: 同じスコアの時は日付順
+      .sort((head1, head2) =>
+        compareDesc(new Date(head1.published), new Date(head2.published)),
+      )
+      // NOTE: 降順にしてほしいため b - a
+      .sort((head1, head2) => score(head2.tags) - score(head1.tags))
+      .slice(0, 5)
+  );
 };

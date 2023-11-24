aa0377ed4a5016f429e96302ec453ef982be290e
Takuya Sakoda <syakoo.pub@gmail.com>
2023-11-24 11:10:26 +0900

    ✨ feat(writings/_models): add type description to type config

--- src/features/writings/_models/writingType.ts
+++ src/features/writings/_models/writingType.ts
@@ -6,9 +6,27 @@ type WritingTypeConfigItem = {
   type: WritingType;
   label: string;
   iconName: IconName;
+  description: string;
 };
 export const writingTypeConfig = [
-  { type: "article", label: "Article", iconName: "document" },
-  { type: "note", label: "Note", iconName: "note" },
-  { type: "diary", label: "Diary", iconName: "book" },
+  {
+    type: "article",
+    label: "Article",
+    iconName: "document",
+    description: "Article は気合いの入った文書を残しています。",
+  },
+  {
+    type: "note",
+    label: "Note",
+    iconName: "note",
+    description:
+      "Note は当時のアイデアやメモなどをアウトプットする目的として残しています。考えや意見は変化する可能性があります。",
+  },
+  {
+    type: "diary",
+    label: "Diary",
+    iconName: "book",
+    description:
+      "Diary は著者の身の回りで起こったことや全く中身のない話を残しています。",
+  },
 ] as const satisfies readonly WritingTypeConfigItem[];

71051a259c952e06c4ce4062e15865c3b02f2dcc
Takuya Sakoda <syakoo.pub@gmail.com>
2024-10-20 15:39:05 +0900

    chore: 🔧 remove console.log

--- src/test/nextjs/env.ts
+++ src/test/nextjs/env.ts
@@ -3,6 +3,4 @@ import { loadEnvConfig } from "@next/env";
 export const loadEnv = () => {
   const projectDir = process.cwd();
   loadEnvConfig(projectDir);
-  console.log("XXXXXXXXXXXXXXX");
-  console.log(process.env.NEXT_PUBLIC_REACTION_SYSTEM_URL);
 };

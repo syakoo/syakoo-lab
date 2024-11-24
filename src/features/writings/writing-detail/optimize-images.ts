2d23435b02eae5595bdcea2c919fa0718a4eb664
Takuya Sakoda <syakoo.pub@gmail.com>
2024-11-24 22:15:53 +0900

    perf: ⚡️ optimize writing image size

---
+++ src/features/writings/writing-detail/optimize-images.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { readFileSync } from "fs";
+
+import { glob } from "glob";
+import type { Sharp } from "sharp";
+import sharp from "sharp";
+
+export const optimizeImages = async () => {
+  const imagePaths = await glob("**/public/img/writings/**/*.{png,jpg,jpeg}");
+
+  for (const imagePath of imagePaths) {
+    const image = sharp(readFileSync(imagePath));
+    const optimizedImage = await optimizeImage(image);
+    if (optimizedImage) {
+      await optimizedImage.toFile(imagePath);
+    }
+  }
+};
+
+const MAX_WIDTH = 2048;
+
+const optimizeImage = async (image: Sharp): Promise<Sharp | undefined> => {
+  const metadata = await image.metadata();
+  if (!metadata.width || metadata.width <= MAX_WIDTH) return;
+
+  return image.resize({ width: MAX_WIDTH });
+};

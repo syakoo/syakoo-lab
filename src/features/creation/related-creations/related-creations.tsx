295456c4f25b22f2cdd6a0cc097600c45874cc91
Takuya Sakoda <syakoo.pub@gmail.com>
2025-07-05 10:10:48 +0900

    refactor(creation): ♻️ optimize related creations data fetching

--- src/features/creation/related-creations/related-creations.tsx
+++ src/features/creation/related-creations/related-creations.tsx
@@ -1,9 +1,6 @@
 import { notFound } from "next/navigation";
 
-import {
-  readCreationById,
-  readCreationSummaries,
-} from "@/features/creation/creation-reader";
+import { readCreationSummaries } from "@/features/creation/creation-reader";
 
 import { getRelatedCreations } from "./get-related-creations";
 import { RelatedCreationsView } from "./related-creations.view";
@@ -13,12 +10,13 @@ type RelatedCreationsProps = {
 };
 
 export const RelatedCreations = async ({ id }: RelatedCreationsProps) => {
-  const targetCreation = await readCreationById(id);
+  const creationSummaries = await readCreationSummaries();
+  const targetCreation = creationSummaries.find((c) => c.id === id);
   if (!targetCreation) notFound();
 
   const relatedCreations = getRelatedCreations(
     targetCreation,
-    await readCreationSummaries(),
+    creationSummaries,
   );
 
   if (relatedCreations.length === 0) {
